f570efdcee5dea15ff842d7434a7c59bc32d0300
Tobias Bruckmeier <tobias.bruckmeier@t-online.de>
2024-07-20 23:25:50 +0200

    not done

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,6 +1,7 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
@@ -9,14 +10,27 @@ use Behat\Gherkin\Node\TableNode;
  */
 class FeatureContext implements Context
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
+    private $conn;
     public function __construct()
     {
+        // Initialisieren der Datenbankverbindung für Tests
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "test_db"; // Test-Datenbank
+
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    /**
+     * @Given I am on the list overview page
+     */
+    public function iAmOnTheListOverviewPage()
+    {
+        $this->visitPath('/1_list_overview.php'); // Pfad zur Seite, auf der das Formular zum Erstellen der Liste ist
     }
 }
